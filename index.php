2566af1b845deb47b6571f67288e0cfb94933dc4
KAS-L1 <kasl.54370906@gmail.com>
2024-09-04 12:59:43 +0800

    deleting html and css adding three folder for css img and js

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <title>To-Do List</title>
 </head>
 <body>
-    <h1>To Do List</h1>
+    <h1>Hello</h1>
 </body>
 </html>
