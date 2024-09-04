457a5e9a3e790044c0610c3bb164307d02a7fe38
KAS-L1 <kasl.54370906@gmail.com>
2024-09-04 23:18:28 +0800

    fixing all bugs ADD REMOVE AND ADDING CHECK

--- app/remove.php
+++ app/remove.php
@@ -1,24 +1,37 @@
 <?php
-
-if(isset($_POST['id'])){
+if(isset($_POST['id']) && !empty($_POST['id'])){
     require '../db_conn.php';
 
     $id = $_POST['id'];
 
-    if(empty($id)){
-       echo 0;
-    }else {
-        $stmt = $conn->prepare("DELETE FROM todos WHERE id=?");
-        $res = $stmt->execute([$id]);
-
-        if($res){
-            echo 1;
-        }else {
-            echo 0;
-        }
-        $conn = null;
-        exit();
+    if(!preg_match('/^\d+$/', $id) || $id == 0){
+       throw new Exception('Invalid id');
     }
+
+    try {
+        $stmt = $conn->prepare("SELECT * FROM todos WHERE id=? LIMIT 1");
+        $stmt->execute([$id]);
+        $result = $stmt->fetch();
+
+        if($result){
+            $stmt = $conn->prepare("DELETE FROM todos WHERE id=? LIMIT 1");
+            $res = $stmt->execute([$id]);
+
+            if($res){
+                echo 1;
+            }else {
+                throw new Exception('Deletion failed');
+            }
+        }else {
+            throw new Exception('Id not found');
+        }
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        echo 0;
+    }
+
+    $conn = null;
+    exit();
 }else {
     header("Location: ../index.php?mess=error");
 }
